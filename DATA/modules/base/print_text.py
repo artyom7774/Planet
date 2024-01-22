1218b396bf6d3ae5e508af26e5b32e047d782260
artyom7774 <120501523+artyom7774@users.noreply.github.com>
2024-01-22 13:35:36 +0300

    Delete DATA directory

--- DATA/modules/base/print_text.py
+++
@@ -1,8 +0,0 @@
-from DATA.modules.variables import *
-import pygame
-
-
-def print_text(screen, x, y, message, font_size, font_type=BASE_FONT, font_color=(0, 0, 0)):
-    font = pygame.font.Font(font_type, font_size)
-    text = font.render(message, True, font_color)
-    screen.blit(text, (x, y))
