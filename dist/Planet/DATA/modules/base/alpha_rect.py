539cf86a62757852ada0089074abc80f56d2123b
unknown <glotij17@gmail.com>
2024-03-11 11:39:01 +0300

    1.3.0

--- dist/Planet/DATA/modules/base/alpha_rect.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-
-def alpha_rect(screen, width, height, x, y, color):
-    surf = pygame.Surface((width, height), pygame.SRCALPHA)
-    surf.fill(color)
-    screen.blit(surf, (x, y))
